5f13add94df6b3c8e8dcebf805ba891d60221311
cw034119 <89610580+cw034119@users.noreply.github.com>
2021-09-02 14:50:52 -0700

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*        File: git-lab-program.cc
+*      Author: Chase Wilson
+*        Date: 09/02/2021
+* Description: Program designed to say Hello to GitHub
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
